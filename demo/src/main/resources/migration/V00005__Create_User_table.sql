0432afe32e93002058f940d6658e0bfe35d0322e
asbVladislav <136428275+asbVladislav@users.noreply.github.com>
2023-06-19 14:40:22 +0300

    Add files via upload

---
+++ demo/src/main/resources/migration/V00005__Create_User_table.sql
@@ -0,0 +1,3 @@
+CREATE TABLE user (login VARCHAR(30), password VARCHAR(255),  role VARCHAR(20));
+insert into user (login,password,role)  values ('firstAdmin','$2a$12$F5YSCf5a9m7OmSjb6mU.SuRenkflpf9b1f9RJ95g5DUUB6YzbP/WK','ADMIN')
+
